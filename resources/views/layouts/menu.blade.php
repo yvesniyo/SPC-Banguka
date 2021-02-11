5d34767d9b06ef9e4274e949c1ef587311523fcd
Niyobuhungiro Yves <niyobuhungiro.yves@gmail.com>
2021-02-11 14:51:16 +0200

    init

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,7 @@
+
+<li class="nav-item {{ Request::is('roles*') ? 'active' : '' }}">
+    <a class="nav-link" href="{{ route('roles.index') }}">
+        <i class="nav-icon icon-cursor"></i>
+        <span>Roles</span>
+    </a>
+</li>
